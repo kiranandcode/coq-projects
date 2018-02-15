78d794cd0292b3aea23c6d436c709b23bcbb621f
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-02-15 21:22:20 +0000

    learning more coq tactics

--- first_proof.v
+++ first_proof.v
@@ -24,3 +24,25 @@ Proof.
        refine (A_implies_B _). 
                 exact proof_of_A.
 Qed.
+
+Theorem backward_large : (forall A B C : Prop, A -> (A -> B) -> (B -> C) -> C).
+Proof.
+        intros A B C.
+        intros proof_of_A A_implies_B B_implies_C.
+        refine (B_implies_C _).
+                refine (A_implies_B _ ).
+                        exact proof_of_A.
+Qed.
+
+
+Theorem backward_huge : (forall A B C : Prop, A -> (A->B) -> (A->B->C) -> C).
+Proof.
+                intros A B C.
+                intros proof_of_A A_implies_B A_imp_B_imp_C.
+                refine (A_imp_B_imp_C _ _).
+                        exact proof_of_A.
+                        refine (A_implies_B _).
+                                exact proof_of_A.
+Qed.
+
+
