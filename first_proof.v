ab9ff0fc1cfd786415ff203f26b033466809e82e
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-02-16 23:42:43 +0000

    More pracitce prooving simple theorems

--- first_proof.v
+++ first_proof.v
@@ -46,3 +46,14 @@ Proof.
 Qed.
 
 
+Theorem forward_huge : (forall A B C : Prop, A -> (A->B) -> (A->B->C) -> C).
+Proof.
+        intros A B C.
+        intros proof_of_A A_implies_B A_imp_B_imp_C.
+        pose (proof_of_B := A_implies_B proof_of_A).
+        pose (proof_of_c := A_imp_B_imp_C proof_of_A proof_of_B).
+        exact proof_of_c.
+Show Proof.
+Qed.
+        
+
