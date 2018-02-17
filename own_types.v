0b236e9258ef2bef64b23082728c1a3e906154c7
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-02-17 00:02:01 +0000

    Doing more proofs of propositions to get the hang of it

--- own_types.v
+++ own_types.v
@@ -30,4 +30,39 @@ Proof.
         case proof_of_false.
 Qed.
 
+Theorem thm_true_imp_true : True -> True.
+Proof.
+        intros proof_of_True.
+        exact I.
+Qed.
 
+
+Theorem thm_false_imp_true : False -> True.
+Proof.
+        intros proof_of_false.
+        exact I.
+Qed.
+
+Theorem thm_false_imp_false : False -> False.
+Proof.
+        intros proof_of_false.
+        case proof_of_false.
+Qed.
+
+Theorem thm_true_imp_false : ~(True -> False).
+Proof.
+        unfold not.
+        intros T_implies_F.
+        refine (T_implies_F _).
+                exact I.
+Qed.
+
+
+Theorem absurd2 : forall A C : Prop, A -> ~ A -> C.
+Proof.
+        intros A C.
+        intros proof_of_A proof_that_A_cannot_be_proven.
+        unfold not in proof_that_A_cannot_be_proven.
+        pose(proof_of_false := proof_that_A_cannot_be_proven proof_of_A).
+        case proof_of_false.
+Qed.
