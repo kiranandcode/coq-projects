6cf5a53ef6b0fe4a3882deaf4bdb815e8b4c3bb1
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-02-18 23:47:31 +0000

    using case statement in function calls with bool

--- bool_type.v
+++ bool_type.v
@@ -6,3 +6,27 @@ Proof.
         exact I.
 Qed.
 
+Theorem False_cannot_be_proven : ~False.
+Proof.
+        unfold not.
+        intros proof_of_false.
+        exact proof_of_false.
+Qed.
+
+
+Theorem not_eqb_true_false: ~(Is_true (eqb true false)).
+Proof.
+        simpl.
+        exact False_cannot_be_proven.
+Qed.
+
+Theorem eqb_a_a : (forall a : bool, Is_true (eqb a a)).
+Proof.
+        intros a.
+        case a.
+        simpl.
+        exact I.
+        simpl.
+        exact I.
+Qed.
+
