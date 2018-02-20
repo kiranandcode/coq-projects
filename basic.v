f1d30a9e93852642189e20b3a8cc9b7058470efb
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-02-20 00:35:24 +0000

    Learning how to apply hypothesis to values

--- basic.v
+++ basic.v
@@ -22,3 +22,22 @@ Proof.
 Qed.
 
 
+Section Predicate_calculus.
+        Variable D : Set.
+        Variable R : D -> D -> Prop.
+Section R_sym_trans.
+Hypothesis R_symmetric: (forall x y : D, R x y -> R y x).
+Hypothesis R_transitive: (forall x y z : D, R x y -> R y z -> R x z).
+
+Lemma refl_if : forall x : D, (exists y , R x y) -> R x x.
+Proof.
+        intros x x_Rlinked.
+        elim x_Rlinked.
+        intros y Rxy.
+        apply R_transitive with y.
+        assumption.
+        apply R_symmetric. 
+        assumption.
+Qed.
+End R_sym_trans.
+
