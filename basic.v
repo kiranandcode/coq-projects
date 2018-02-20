ffb7ae04091382f989ec8562ff240f9bade6fd22
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2018-02-20 00:28:20 +0000

    looking at auto and trivial and tauto for quick solving.

--- basic.v
+++ basic.v
@@ -1,5 +1,6 @@
 
 Theorem and_commutative : (forall A B :Prop,  A /\ B -> B /\ A).
+Proof.
 intros.
 elim H.
 intros.
@@ -7,4 +8,17 @@ split.
 auto.
 auto.
 Qed.
+
+
+Theorem or_commutative : (forall A B : Prop, A \/ B -> B \/ A).
+Proof.
+        intros.
+        elim H.
+        intro HA.
+        clear H. (* remove unnecessary proofs *)
+        right.
+        trivial.
+        auto.
+Qed.
+
 
